248b37145c3293258360aad1102312283942b04c
Bryan Smith <bryan97smith@gmail.com>
2024-04-15 23:33:24 +0200

    fix for webapp deployment

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ const app = express();
 app.use(express.json());
 
 function addHF(filePath) {
-    const head = fs.readFileSync(path.join(__dirname, 'views', 'head.html'), 'utf8');
-    const header = fs.readFileSync(path.join(__dirname, 'views', 'header.html'), 'utf8');
-    const footer = fs.readFileSync(path.join(__dirname, 'views', 'footer.html'), 'utf8');
+    const head = fs.readFileSync(path.join('/home/site/wwwroot', 'views', 'head.html'), 'utf8');
+    const header = fs.readFileSync(path.join('/home/site/wwwroot', 'views', 'header.html'), 'utf8');
+    const footer = fs.readFileSync(path.join('/home/site/wwwroot', 'views', 'footer.html'), 'utf8');
     const originalContent = fs.readFileSync(filePath, 'utf8');
     return head + header + originalContent + footer;
 }
 
 // app.use('/', express.static('/home/site/wwwroot', {index: 'index.html'}));
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join('/home/site/wwwroot')));
 
 // Routes
-app.get('/', (req, res) => {res.send(addHF(path.join(__dirname, 'views', 'index.html')));});
-app.get('/login', (req, res) => {res.send(addHF(path.join(__dirname, 'views', 'login.html')));});
+app.get('/', (req, res) => {res.send(addHF(path.join('/home/site/wwwroot', 'views', 'index.html')));});
+app.get('/login', (req, res) => {res.send(addHF(path.join('/home/site/wwwroot', 'views', 'login.html')));});
 
 // API routes
 const users = require('./api/users');
