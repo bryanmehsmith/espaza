f504b2c390e7f3d07b18b8d089c9919156837be3
Bryan Smith <bryan97smith@gmail.com>
2024-05-21 18:24:07 +0200

    re-implement savechanges

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,3 +1,20 @@
+function saveChanges(itemId, payload) {
+  fetch(`/cart/${itemId}`, {
+      method: 'PUT',
+      headers: {
+      'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+    var totalCell = document.getElementById('product-total-' + itemId);
+    totalCell.textContent = payload.quantity * document.getElementById('product-price-' + itemId).textContent;
+})
+  .catch((error) => console.error('Error:', error));
+}
+
 function addToCart(userId, itemId) {
   fetch(`/cart`, {
       method: 'POST',
@@ -51,26 +68,26 @@ fetch('/cart/items')
   let totalPrice = 0.00;
 
   const productRows = products.map(product => `
-    <tr id="product-row-${product.itemId}">
-      <td>
-       ${product.name}
-      </td>
-      <td>
-       ${product.price}
-      </td>
-      <td>
-        ${product.quantity}
-      </td>
-      <td>
-      ${product.quantity * product.price}
-    </td>
-      <td>
-      <button type="submit" class = "btn d-flex m-2 py-2 fa-2x" style= "color:rgb(175, 21, 21);text-shadow:2px 2px 4px #9b9b9b;">
-          <i class="bi bi-trash" onclick="removeFromCart('${product.itemId}')"></i>
-        </button>
-      </td>
-    </tr>
-  `).join('');
+      <tr id="product-row-${product.itemId}">
+        <td>
+         ${product.name}
+        </td>
+        <td id="product-price-${product.itemId}">
+         ${product.price}
+        </td>
+        <td>
+          <input type="number" id="productQuantity-${product.itemId}" value="${product.quantity}" onChange="saveChanges('${product.itemId}', {'quantity' : this.value})">
+        </td>
+        <td id="product-total-${product.itemId}">
+          ${product.quantity * product.price}
+        </td>
+        <td>
+        <button type="submit" class = "btn d-flex m-2 py-2 fa-2x" style= "color:rgb(175, 21, 21);text-shadow:2px 2px 4px #9b9b9b;">
+            <i class="bi bi-trash" onclick="removeFromCart('${product.itemId}')"></i>
+          </button>
+        </td>
+      </tr>
+    `).join('');
 
   document.querySelector('tbody').innerHTML = productRows;
 
